175d52bff805f21f7516173c571bd71ca7a924de
Camil Grabowski <camil.grabowski@uconn.edu>
2021-10-06 11:13:22 -0400

    almost done

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,7 +3,7 @@ const { Thought, User } = require("../models");
 const thoughtController = {
   getAllThoughts(req, res) {
     Thought.find({})
-      .select(-__v)
+      .select('-__v')
       .sort({ _id: -1 })
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
